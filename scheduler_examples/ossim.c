dc4aff35f0a3a2628a00b15bc6206f79801fd1ba
Nádia Matias <[nadia.r.c.matias@gmaol.com]>
2025-10-02 09:31:32 +0100

    Addicionei um SJF (mas ainda é FIFO)

--- scheduler_examples/ossim.c
+++ scheduler_examples/ossim.c
@@ -16,6 +16,7 @@
 
 #include "msg.h"
 #include "queue.h"
+#include "sjf.h"
 
 static uint32_t PID = 0;
 
@@ -233,11 +234,9 @@ void check_blocked_queue(queue_t * blocked_queue, queue_t * command_queue, uint3
 
 static const char *SCHEDULER_NAMES[] = {
     "FIFO",
-/*
     "SJF",
     "RR",
     "MLFQ",
-*/
     NULL
 };
 
@@ -309,6 +308,9 @@ int main(int argc, char *argv[]) {
         switch (scheduler_type) {
             case SCHED_FIFO:
                 fifo_scheduler(current_time_ms, &ready_queue, &CPU);
+                break;
+            case SCHED_SJF:
+                sjf_scheduler(current_time_ms, &ready_queue, &CPU);
                 break;
 
             default:
